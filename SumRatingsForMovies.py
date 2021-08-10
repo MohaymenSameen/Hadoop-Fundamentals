7837f20c8e0ada208e6b9ef637f5f82c1b9fb998
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-08-10 17:56:04 +0200

    Assignment 1 Sum and sort movie ratings

--- SumRatingsForMovies.py
+++ SumRatingsForMovies.py
@@ -5,16 +5,30 @@ from mrjob.step import MRStep
 class SumRatingsForMovies(MRJob):
 
     def steps(self):
-        return [MRStep(mapper=self.mapper_get_rating,
-                       reducer=self.reducer_count_ratings)
-                ]
+        return [
+            MRStep(
+                mapper=self.mapper_get_movies, combiner=self.combine_movie_rating_counts,
+                reducer=self.reducer_sum_rating_counts
+            ),
+            MRStep(
+                reducer=self.reducer_sort_counts
+            )
+        ]
 
-    def mapper_get_rating(self, _, line):
+    def mapper_get_movies(self, _, line):
         (userID, movieID, rating, timestamp) = line.split('\t')
+
         yield movieID, 1
 
-    def reducer_count_ratings(self, key, values):
+    def combine_movie_rating_counts(self, key, values):
         yield key, sum(values)
+
+    def reducer_sum_rating_counts(self, key, values):
+        yield None, (sum(values), key)
+
+    def reducer_sort_counts(self, _, values):
+        for count, key in sorted(values, reverse=True):
+            yield int(count), key
 
 
 if __name__ == '__main__':
